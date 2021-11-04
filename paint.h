eb5df953c9d71b969a15e4d506aa7435f3b00641
DenisLBD <89631756+DenisLBD@users.noreply.github.com>
2021-11-04 13:51:06 +0300

    Add files via upload

---
+++ paint.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QFrame>
+
+class Frame : public QFrame
+{
+
+Q_OBJECT
+
+public:
+    Frame(QList<std::pair<int, int>> listOfCoord, QWidget *parent = 0);
+
+public:
+    void paintEvent(QPaintEvent *e);
+    void drawLines();
+
+private:
+    QList<std::pair<int, int>> CoordList;
+};
